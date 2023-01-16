bf9936748b4a451cbf0fd2769d22926ca06df9df
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-01-17 00:23:20 +0400

    upd

--- 1.2/variables/var.js
+++ 1.2/variables/var.js
@@ -5,3 +5,44 @@ const yourSecondVariable = 10;
 const yourThirdVariable = { FirstProperty: 'hello world' };
 
 console.log(yourFirstVariable);
+
+// 
+// const newVariable = 'hello world';
+
+
+// let anotherVariable;
+
+// anotherVariable = 20;
+
+// console.log(anotherVariable);
+
+const variable1 = 10;
+
+let variable2 = 10;
+
+var variable3 = 10;
+
+//////////////////////////// var can be redecleared
+
+var myVariable = 10;
+
+var myVariable = 20;
+
+
+/////////////////////// let can be reassigned 
+
+let counter = 0;
+
+counter = counter + 1; // counter++
+
+///////// var naming standarts
+
+// const yourFirstVariable = 10;
+const your_first_variable = 10;
+const YOUR_FIRST_VARIABLE = 10;
+const yourfirstvariable = 10;
+const YourFirstVariable = 10;
+
+// Naming Conventions for different companies
+
+// for example google, airbnb
