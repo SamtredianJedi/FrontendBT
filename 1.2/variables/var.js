c92cb068c834aa09eeb59d429a30284cefd67cd0
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-01-16 23:50:00 +0400

    var

---
+++ 1.2/variables/var.js
@@ -0,0 +1,7 @@
+const yourFirstVariable = 'Learning to code gives you superpowers';
+
+const yourSecondVariable = 10;
+
+const yourThirdVariable = { FirstProperty: 'hello world' };
+
+console.log(yourFirstVariable);
